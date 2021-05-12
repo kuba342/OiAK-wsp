a7df0ab75f79e4aa6aa01f99eee5dd8c75f1e4de
https://github.com/kuba342 <j.derda010@gmail.com>
2021-05-12 21:13:45 +0200

    functions - reszta z table

--- functions.py
+++ functions.py
@@ -3,29 +3,48 @@ import math
 def rtz_Xplus(d, n):
     result = d * math.floor((2**n)/d) - 1
     return result
-    pass
 
 def fr_Xplus(d, n):
     result = math.floor((2**n)/d)
     return result
-    pass
 
 def rte_Xplus(d,n):
     result = d * math.floor((2**(n+1) - d - 1)/(2*d)) - 1
     return result
-    pass
 
 def rtz_Xminus(d,n):
     result = d* math.floor((2**n)/d) - d + 1
     return result
-    pass
 
 def fr_Xminus(d,n):
     result = math.floor((2 ** n) / d)
     return result
-    pass
 
 def rte_Xminus(d,n):
     result = d * math.floor((2**(n+1) - d - 3)/(2*d)) + 1
     return result
-    pass
+
+def rtz_Yplus():
+    return (0,0)
+def rtz_Yminus(k,a,d,n):
+    result_1 = ( (2**k - a*d) * math.floor((2**n)/d) )
+    result_2 = (2**k - a*(d-1) - 1)
+    return result_1, result_2
+
+def fr_Yplus():
+    return (0,0)
+def fr_Yminus(k,a,d,n):
+    result_1 = (2**k - a*d) * math.floor((2**n)/d)
+    result_2 = (2**k) - 1
+    return result_1, result_2
+
+def rte_Yplus(k,a,d,n):
+    # Sprawdzić tą funkcję
+    result_1 = int((a*(d-1))/2 + (2**k - a*d))
+    result_2 = int((a*(d-1))/2 + (2**k - a*d) * math.floor((2**(n+1) + d - 1)/(2*d)) - 1)
+    return result_1, result_2
+
+def rte_Yminus(k,a,d,n):
+    result_1 = int( (a*(d-1)/2) + (2**k - a*d) * math.floor( (2**(n+1) + d - 3) / (2*d) ) - 1 )
+    result_2 = int( (a*(d+1))/2 + 2**k - a*d - 1 )
+    return result_1, result_2
